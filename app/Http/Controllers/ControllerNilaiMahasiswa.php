d48c5052198b13adab6e06b47134fcc2fcced9a1
Ahan18-cell <muhammad.raeihan@mhs.politala.ac.id>
2025-10-04 23:12:40 +0800

    updete route

--- app/Http/Controllers/ControllerNilaiMahasiswa.php
+++ app/Http/Controllers/ControllerNilaiMahasiswa.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class ControllerNilaiMahasiswa extends Controller
 {
     public function index(){
-        
+
+     return view('nilai-mahasiswa.index');   
     }
 
 }
