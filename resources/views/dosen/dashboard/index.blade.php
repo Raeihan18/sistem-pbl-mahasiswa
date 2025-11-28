fa175f5be34a8a7877f5011b51da1952722719a3
Khaidir Ramadhan <ramadhankhaidir214@.com>
2025-11-28 10:09:27 +0800

    update dashboard

--- resources/views/dosen/dashboard/index.blade.php
+++ resources/views/dosen/dashboard/index.blade.php
@@ -4,16 +4,6 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="d-flex align-items-center mb-2">
-            <form class="ms-auto d-none d-sm-inline-block mw-200" style="width: 250px;">
-                <div class="input-group">
-                    <input type="text" class="form-control bg-light small" placeholder="Search for..." aria-label="Search">
-                    <button class="btn btn-primary" type="button">
-                        <i class="bi bi-search"></i>
-                    </button>
-                </div>
-            </form>
-        </div>
         <!-- Ringkasan Statistik -->
         <div class="row">
 
